b9ec1eec86bdbc33544d6cf201f686a1b8f65657
u4usmangenius <126275034+u4usmangenius@users.noreply.github.com>
2023-08-25 23:54:18 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
-
-const { app, BrowserWindow, session } = require("electron");
+const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 600,
     height: 500,
-    title: "Calculator",
-    resizable: false, 
+    title:"Calculator",
+    resizable: false,
     webPreferences: {
       nodeIntegration: true,
     },
-  });
-
-  const winSession = win.webContents.session;
-  winSession.webRequest.onHeadersReceived((details, callback) => {
-    const headers = Object.assign({}, details.responseHeaders, {
-      "Content-Security-Policy":
-        "default-src 'self'; script-src 'self' 'unsafe-inline'",
-    });
-    callback({ cancel: false, responseHeaders: headers });
   });
 
   win.loadFile("index.html");
